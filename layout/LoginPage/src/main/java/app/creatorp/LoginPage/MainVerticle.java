66de90ace1270585fedd7460b62eb10457c70b77
Parsaloi <parsaloi35@gmail.com>
2023-07-30 15:44:09 +0300

    Dart for the web..ahah, TypeScript for the web..good enough...

--- layout/LoginPage/src/main/java/app/creatorp/LoginPage/MainVerticle.java
+++ layout/LoginPage/src/main/java/app/creatorp/LoginPage/MainVerticle.java
@@ -5,21 +5,21 @@ import io.vertx.core.Promise;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.StaticHandler;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+  // private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
 
-	  LOGGER.info("Starting the application...")
+	  // LOGGER.info("Starting the application...")
 
 	  Router router = Router.router(vertx);
 
-	  router.route("/main/resources/webroot").handler(StaticHandler.create());
+	  router.route().handler(StaticHandler.create("resources/webroot/"));
 	  router.get("/*").handler(ctx -> ctx.reroute("/index.html"));
 
 	  vertx.createHttpServer()
